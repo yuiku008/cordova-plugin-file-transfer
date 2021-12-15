e23366c290a9e4bc1270847574fea8eadcd8aedd
Shawn Sha <sharwnsha@knx.com.cn>
2021-12-15 13:53:23 +0800

    去掉依赖

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,8 +5,7 @@
 //
 // Copyright (c) Microsoft Open Technologies Inc
 // Licensed under the MIT license
-
-/// <reference types="cordova-plugin-file" />
+ 
 
 /**
  * The FileTransfer object provides a way to upload files using an HTTP multi-part POST request,
